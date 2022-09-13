2cc4ec9417c77d012e1e7f8b0e585a8cc7b1a760
stefan504 <63974903+stefan504@users.noreply.github.com>
2022-09-13 16:16:13 +0200

    css adjustments and additional page for benefits

---
+++ src/components/Benefits/BenefitsShowcase.tsx
@@ -0,0 +1,6 @@
+import './BenefitsShowcase.css';
+const BenefitsShowcase = () => {
+	return <div className="bnf">BenefitsShowcase</div>;
+};
+
+export default BenefitsShowcase;
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
-
+import BenefitsShowcase from '../Benefits/BenefitsShowcase';
 import './Home.css';
 
 const Home = () => {
 	return (
-		<div className="home-container">
-			<div className="home-text">
-				<h1>
-					TRACK YOUR PROGRESS.<br></br>MAKE GAINS.
-				</h1>
-				<div className="button-container">
-					<Link className="button" to="/tracker">
-						Join the Club
-					</Link>
-					<Link className="button button-second" to="/tracker">
-						Try Tracker
-					</Link>
+		<div className="container">
+			{' '}
+			<div className="home-container">
+				<div className="home-text">
+					<h1>
+						TRACK YOUR<span className="yellow-letters"> PROGRESS</span>.
+						<br></br>MAKE <span className="yellow-letters">GAINS</span>.
+					</h1>
+					<div className="button-container">
+						<Link className="button" to="/tracker">
+							Workout Plans
+						</Link>
+						<Link className="button button-second" to="/tracker">
+							Tracker
+						</Link>
+					</div>
 				</div>
 			</div>
+			<BenefitsShowcase />
 		</div>
 	);
 };
