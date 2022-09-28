d3bd99e3e59137f22bb777df7131e00bc02210e2
stefan504 <63974903+stefan504@users.noreply.github.com>
2022-09-28 11:49:10 +0200

    simplify some of logic, remove redundant code

--- src/components/Track/Track.tsx
+++ src/components/Track/Track.tsx
@@ -18,93 +18,47 @@ const Track: React.FC<FilteredStateTypes> = ({
 		setTrack(newTrack);
 	};
 
-	if (filteredState === null) {
-		return (
-			<div className="track">
-				{track?.map((trac: any) => {
-					return (
-						<div
-							key={Math.floor(Math.random() * 120000)}
-							className="track-container"
-						>
-							<span onClick={(e) => deleteTrack(e, trac.id)} className="delete">
-								&#10005;
-							</span>
-							<span className="color"></span>
-							<div className="info">
-								<h2>
-									Muscle group:{' '}
-									<span className="color">{trac.muscleGroup}</span>
-								</h2>
-								<h3>
-									Exercise: <span className="color">{trac.exercise}</span>
-								</h3>
-								<h4>
-									Weight: <span className="color">{trac.weight}kg</span>
-								</h4>
-								<p>
-									Sets: <span className="color">{trac.sets}</span>
-								</p>
-								<p>
-									Reps: <span className="color">{trac.reps}</span>
-								</p>
-							</div>
-							<div className="date">
-								<h3>
-									<span className="color">{trac.date}</span>
-								</h3>
-							</div>
+	const isFiltered = filteredState === null ? track : filteredState;
+
+	return (
+		<div className="track">
+			{isFiltered?.map((trac: any) => {
+				return (
+					<div
+						key={Math.floor(Math.random() * 120000)}
+						className="track-container"
+					>
+						<span onClick={(e) => deleteTrack(e, trac.id)} className="delete">
+							&#10005;
+						</span>
+						<span className="color"></span>
+						<div className="info">
+							<h2>
+								Muscle group: <span className="color">{trac.muscleGroup}</span>
+							</h2>
+							<h3>
+								Exercise: <span className="color">{trac.exercise}</span>
+							</h3>
+							<h4>
+								Weight: <span className="color">{trac.weight}kg</span>
+							</h4>
+							<p>
+								Sets: <span className="color">{trac.sets}</span>
+							</p>
+							<p>
+								Reps: <span className="color">{trac.reps}</span>
+							</p>
 						</div>
-					);
-				})}
-			</div>
-		);
-	} else if (filteredState.length > 0) {
-		return (
-			<div className="track">
-				{filteredState.map((filter: any) => {
-					return (
-						<div
-							key={Math.floor(Math.random() * 120000)}
-							className="track-container"
-						>
-							<span
-								onClick={(e) => deleteTrack(e, filter.id)}
-								className="delete"
-							>
-								&#10005;
-							</span>
-							<span className="color"></span>
-							<div className="info">
-								<h2>
-									Muscle group:{' '}
-									<span className="color">{filter.muscleGroup}</span>
-								</h2>
-								<h3>
-									Exercise: <span className="color">{filter.exercise}</span>
-								</h3>
-								<h4>
-									Weight: <span className="color">{filter.weight}kg</span>
-								</h4>
-								<p>
-									Sets: <span className="color">{filter.sets}</span>
-								</p>
-								<p>
-									Reps: <span className="color">{filter.reps}</span>
-								</p>
-							</div>
-							<div className="date">
-								<h3>
-									<span className="color">{filter.date}</span>
-								</h3>
-							</div>
+						<div className="date">
+							<h3>
+								<span className="color">{trac.date}</span>
+							</h3>
 						</div>
-					);
-				})}
-			</div>
-		);
-	}
-	return null;
+					</div>
+				);
+			})}
+		</div>
+	);
 };
 
 export default Track;
